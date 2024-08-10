2bf78f5aa6ab9109d03f0bbb124b76f8e7c20cb9
The-eulogyowl <bvunderink1@gmail.com>
2024-08-10 14:59:07 +0000

    started adding dynamic data and using bootstrap for styling

--- app.py
+++ app.py
@@ -2,8 +2,35 @@ from flask import Flask, render_template
 
 app=Flask(__name__)
 
+Projects=[
+  {
+    'id':1,
+    'title':'Project 1',
+    'description':'This is a project 1',
+    'finish date':'2-9-2024'
+  },
+  {
+    'id':2,
+    'title':'Project 1',
+    'description':'This is a project 1',
+    'finish date':'2-9-2024'
+  },
+  {
+    'id':3,
+    'title':'Project 1',
+    'description':'This is a project 1',
+    'finish date':'2-9-2024'
+  },
+  {
+    'id':4,
+    'title':'Project 1',
+    'description':'This is a project 1',
+    'finish date':'2-9-2024'
+  }
+]
+
 @app.route("/")
 def Hello_world():
-  return render_template('home.html')
+  return render_template('home.html',projects=Projects)
 if __name__=="__main__":
   app.run(host='0.0.0.0',debug=True)
